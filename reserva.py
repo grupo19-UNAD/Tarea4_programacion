f80129f1a7858e73da0226c4f97b930317c1bd2f
Celeste Aconcha <celesteaconcha@gmail.com>
2026-05-11 15:30:20 -0500

    Lógica de servicios y reservas

---
+++ reserva.py
@@ -0,0 +1,31 @@
+# reserva.py
+import logging
+from excepciones import ReservaInvalidaError, SoftwareFJError
+
+class Reserva:
+    def __init__(self, cliente, servicio, duracion):
+        self.cliente = cliente
+        self.servicio = servicio
+        self.duracion = duracion
+        self.estado = "CREADA"
+
+    def procesar(self, **parametros_dinamicos):
+        try:
+            if self.duracion <= 0:
+                raise ReservaInvalidaError(f"La duración ({self.duracion}) es inválida.")
+            
+            total = self.servicio.calcular_costo(self.duracion, **parametros_dinamicos)
+            self.estado = "CONFIRMADA"
+            logging.info(f"ÉXITO: ID_Cliente {self.cliente.id_entidad} reservó {self.servicio.nombre}. Total: ${total}")
+            return total
+
+        except ReservaInvalidaError as e:
+            self.estado = "CANCELADA POR ERROR"
+            logging.error(f"Error en Reserva: {e}")
+            raise
+        except Exception as e:
+            self.estado = "FALLO TÉCNICO"
+            logging.critical(f"Fallo crítico inesperado: {e}")
+            raise SoftwareFJError("El sistema de reservas colapsó internamente.") from e
+        finally:
+            print(f">>> Finalización de proceso. Estado: {self.estado}\n")
---
+++ servicios.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+from excepciones import ValidacionDatosError
+
+
+class Servicio(ABC):
+    def __init__(self, nombre, costo_base):
+        if costo_base < 0:
+            raise ValidacionDatosError("El costo base no puede ser negativo.")
+        self.nombre = nombre
+        self.costo_base = costo_base
+        
+    @abstractmethod
+    def calcular_costo(self, cantidad, **kwargs):
+        pass
+
+    # Añadimos este método para fortalecer el Polimorfismo
+    def __str__(self):
+        return f"Servicio: {self.nombre} | Costo Base: ${self.costo_base}"
+
+class SalaReunion(Servicio):
+    def calcular_costo(self, horas, descuento=0):
+        total = (self.costo_base * horas) - descuento
+        return max(total, 0)
+
+    def obtener_detalles(self):
+        return f"SALA: {self.nombre} (Costo/Hora: ${self.costo_base})"
+
+class AlquilerEquipo(Servicio):
+    def calcular_costo(self, dias, tiene_seguro=True):
+        costo = self.costo_base * dias
+        if tiene_seguro:
+            costo += 25.0
+        return costo
+
+    def obtener_detalles(self):
+        return f"EQUIPO: {self.nombre} (Costo/Día: ${self.costo_base})"
+
+class AsesoriaEspecializada(Servicio):
+    def calcular_costo(self, sesiones):
+        if sesiones >= 5:
+            return (self.costo_base * sesiones) * 0.80
+        return self.costo_base * sesiones
+
+    def obtener_detalles(self):
+        return f"ASESORÍA: {self.nombre} (Costo/Sesión: ${self.costo_base})"
